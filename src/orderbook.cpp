c6f35d6c64f2d0448b7cf373ace823ae7995ff9c
Evan Phillips <evanap003300@gmail.com>
2026-03-21 00:30:06 -0400

    impl. continous orderbook

--- src/orderbook.cpp
+++ src/orderbook.cpp
@@ -1,11 +1,103 @@
 #include "orderbook.hpp"
 
-void OrderBook::runExchange() {
-    // Read orders from ITCH feed
+std::vector<ItchOrderExecuted> OrderBook::handleBuyOrder(Order& buyOrder) {
+    std::vector<ItchOrderExecuted> executedOrders;
 
-    // Add order to order book
+    // loop through all the asks and match orders
+    while (buyOrder.shares > 0 && !asks.empty()) {
+        auto& [price, currentAsks] = *asks.begin();
 
-    // Match orders 
+        if (price > buyOrder.price) {
+            break;
+        }
 
-    // Restart 
+        while (buyOrder.shares > 0 && !currentAsks.empty()) {
+            auto& sellOrder = currentAsks.front();
+            uint32_t executedShares = std::min(buyOrder.shares, sellOrder.shares);
+            ItchOrderExecuted executedOrder;
+            executedOrder.message_type = 'E';
+            executedOrder.stock_locate = buyOrder.stockLocate;
+            executedOrder.tracking_number = 0;
+            executedOrder.timestamp_high = 0; // get the current time here
+            executedOrder.timestamp_low = 0;
+            executedOrder.order_reference_number = (static_cast<uint64_t>(sellOrder.orderReferenceNumberHigh) << 32) | sellOrder.orderReferenceNumberLow;
+            executedOrder.executed_shares = executedShares;
+            executedOrder.match_number = 0; 
+            executedOrders.push_back(executedOrder);
+
+            buyOrder.shares -= executedShares;
+            sellOrder.shares -= executedShares;
+
+            if (sellOrder.shares == 0) {
+                currentAsks.pop_front();
+            }
+        }
+
+        if (currentAsks.empty()) {
+            asks.erase(asks.begin());
+        }
+    }
+
+    // Add order if not empty 
+    if (buyOrder.shares > 0) {
+        bids[buyOrder.price].push_back(buyOrder);
+    }
+
+    return executedOrders;
+}
+
+std::vector<ItchOrderExecuted> OrderBook::handleSellOrder(Order& sellOrder) {
+    std::vector<ItchOrderExecuted> executedOrders;
+
+    // loop through all the bids and match orders
+    while (sellOrder.shares > 0 && !bids.empty()) {
+        auto& [price, currentBids] = *bids.begin();
+
+        if (price < sellOrder.price) {
+            break;
+        }
+
+        while (sellOrder.shares > 0 && !currentBids.empty()) {
+            auto& buyOrder = currentBids.front();
+            uint32_t executedShares = std::min(sellOrder.shares, buyOrder.shares);
+            ItchOrderExecuted executedOrder;
+            executedOrder.message_type = 'E';
+            executedOrder.stock_locate = sellOrder.stockLocate;
+            executedOrder.tracking_number = 0;
+            executedOrder.timestamp_high = 0; // get the current time here
+            executedOrder.timestamp_low = 0;
+            executedOrder.order_reference_number = (static_cast<uint64_t>(buyOrder.orderReferenceNumberHigh) << 32) | buyOrder.orderReferenceNumberLow;
+            executedOrder.executed_shares = executedShares;
+            executedOrder.match_number = 0; 
+            executedOrders.push_back(executedOrder);
+
+            buyOrder.shares -= executedShares;
+            sellOrder.shares -= executedShares;
+
+            if (buyOrder.shares == 0) {
+                currentBids.pop_front();
+            }
+        }
+
+        if (currentBids.empty()) {
+            bids.erase(bids.begin());
+        }
+    }
+
+    // Add order if not empty 
+    if (sellOrder.shares > 0) {
+        asks[sellOrder.price].push_back(sellOrder);
+    }
+
+    return executedOrders;
+}
+
+std::vector<ItchOrderExecuted> OrderBook::handleOrder(Order& order) {
+    if (order.buySellIndicator == 'B') {
+        return handleBuyOrder(order);
+    } else if (order.buySellIndicator == 'S') {
+        return handleSellOrder(order);
+    } else {
+        throw std::runtime_error("Invalid buy/sell indicator");
+    }
 }
--- src/orderbook.hpp
+++ src/orderbook.hpp
@@ -1,8 +1,27 @@
 #pragma once
 #include "itch.hpp"
-#include "order_matching.hpp"
+#include <stdint.h>
+#include <map>                                                                                                                                                                   
+#include <deque> 
+
+struct ItchOrderExecuted {
+    char message_type;       
+    uint16_t stock_locate;   
+    uint16_t tracking_number; 
+    uint16_t timestamp_high;  
+    uint32_t timestamp_low;
+    uint64_t order_reference_number; 
+    uint32_t executed_shares;   
+    uint64_t match_number;      
+};
 
 class OrderBook {
 public: 
-    void runExchange();
+    std::vector<ItchOrderExecuted> handleOrder(Order& order);
+
+private: 
+    std::map<uint32_t, std::deque<Order>, std::greater<uint32_t>> bids;    
+    std::map<uint32_t, std::deque<Order>> asks;
+    std::vector<ItchOrderExecuted> handleBuyOrder(Order& order);
+    std::vector<ItchOrderExecuted> handleSellOrder(Order& order);
 };
