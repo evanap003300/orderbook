60c5eb3901a33760d5e9aff826b540576a494248
Evan Phillips <evanap003300@gmail.com>
2026-03-21 12:35:53 -0400

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "matching_engine.hpp"
+#include <iostream>
 
 int main() {    
+    std::cout << "Generating synthetic ITCH data...\n";
     ItchParser parser;
-    parser.generateItch(1000); // Generate 1000 synthetic orders for testing
+    parser.generateItch(10000);
 
+    std::cout << "Running matching engine...\n";
     MatchingEngine engine;
     engine.run();
     
