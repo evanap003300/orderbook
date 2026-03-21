46be572a52a1d58ad970d41ce44af785ed4914d1
Evan Phillips <evanap003300@gmail.com>
2026-03-21 12:38:56 -0400

    added time testing

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,19 @@
 
 int main() {    
     std::cout << "Generating synthetic ITCH data...\n";
+    const uint32_t numOrders = 10000;
     ItchParser parser;
-    parser.generateItch(10000);
+    parser.generateItch(numOrders);
 
     std::cout << "Running matching engine...\n";
     MatchingEngine engine;
+
+    auto start = std::chrono::high_resolution_clock::now();
     engine.run();
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto elapsed = end - start;
+    std::cout << "Matching engine completed in " << elapsed.count() << " milliseconds.\n";
     
     return 0;
 }
