e533b04a80d5d0adf969c57a19571e861313e764
Evan Phillips <evanap003300@gmail.com>
2026-03-22 16:13:56 -0400

    set up tests with cmake

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,3 +4,19 @@
     * The tests cover various scenarios such as order matching, partial fills,
     * and edge cases to ensure the correctness of the matching engine implementation.
 */
+#include <gtest/gtest.h>
+#include "itch.hpp"
+#include "matching_engine.hpp"
+#include "orderbook.hpp"
+
+TEST(MatchingEngineTest, BasicMatching) {
+    
+}
+
+TEST(MatchingEngineTest, partialFill) {
+    
+}
+
+TEST(MatchingEngineTest, stockLeftover) {
+    
+}
